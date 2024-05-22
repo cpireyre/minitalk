4bcff0e57ebe5a9c2452a4ae26d3717719b2bfcd
Colin Pireyre <copireyr@student.hive.fi>
2024-05-22 11:30:19 +0300

    refactor

--- src/server.c
+++ src/server.c
@@ -6,21 +6,20 @@
 /*   By: copireyr <copireyr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 10:19:59 by copireyr          #+#    #+#             */
-/*   Updated: 2024/05/21 12:41:28 by copireyr         ###   ########.fr       */
+/*   Updated: 2024/05/22 11:29:55 by copireyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	set_signal_handlers(t_signal_handler callback);
+static void	set_signal_handlers(t_signal_handler callback);
+static void	receive_msg(size_t msg_size);
 
 int	main(void)
 {
 	size_t	msg_size;
-	char	*msg;
 	void	*ret;
 
-	ft_dprintf(STDERR_FILENO, "Initialising server...\n");
 	set_signal_handlers(&handler);
 	ft_printf("Server PID: %u.\n", getpid());
 	while (1)
@@ -30,25 +29,12 @@ int	main(void)
 		msg_size = 0;
 		ret = receive(&msg_size, sizeof(msg_size));
 		if (ret && msg_size)
-		{
-			msg = malloc(msg_size + 1);
-			if (!msg)
-			{
-				ft_dprintf(STDERR_FILENO, "ERROR: malloc fail\n");
-				return (1);
-			}
-			msg[msg_size] = '\0';
-			if (receive(msg, msg_size))
-				ft_printf("「 %s 」\n", msg);
-			else
-				ft_dprintf(STDERR_FILENO, ERROR_CLIENT_TIMED_OUT);
-			free(msg);
-		}
+			receive_msg(msg_size);
 	}
 	return (0);
 }
 
-void	set_signal_handlers(t_signal_handler callback)
+static void	set_signal_handlers(t_signal_handler callback)
 {
 	struct sigaction	action;
 
@@ -59,4 +45,22 @@ void	set_signal_handlers(t_signal_handler callback)
 	sigaddset(&(action.sa_mask), SIGUSR2);
 	sigaction(SIGUSR1, &action, NULL);
 	sigaction(SIGUSR2, &action, NULL);
+}
+
+static void	receive_msg(size_t msg_size)
+{
+	char	*msg;
+
+	msg = malloc(msg_size + 1);
+	if (msg)
+	{
+		msg[msg_size] = '\0';
+		if (receive(msg, msg_size))
+			ft_printf("「 %s 」\n", msg);
+		else
+			ft_dprintf(STDERR_FILENO, ERROR_CLIENT_TIMED_OUT);
+		free(msg);
+	}
+	else
+		ft_dprintf(STDERR_FILENO, "ERROR: malloc fail\n");
 }
